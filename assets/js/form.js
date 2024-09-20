6ea33f37df78ddff2412ff826b61f110afa7bc66
Crispin Duarte <CrispinDuarte1@outlook.com>
2024-09-19 19:50:24 -0700

    fixed file issues

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,18 +1,16 @@
 const plInput = document.getElementById('plName');
-const stInput = document.getElementById('stAddess');
+const stInput = document.getElementById('stAddress');
 const cityInput = document.getElementById('city');
-const stateInput = document.getElementById('state');
 const zipCodeInput = document.getElementById('zipCode');
 const submitButton = document.getElementById('submit');
 
-submitButton.addEventListener('submit', function(event) {
+submitButton.addEventListener('click', function(event) {
     event.preventDefault();
 
     const userData = {
         plname: plInput.value,
         staddress: stInput.value,
         city: cityInput.value,
-        state: stateInput.value,
         zipcode: zipCodeInput.value
     };
 
